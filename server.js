a6869ad3c7df0441d6cefa337ec6cb9e13a27eea
Jackson Debbarma <jacksondebbarma@Jacksons-MacBook-Pro.local>
2022-04-01 00:43:00 +0530

    MongoDB connected

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-const express = require('express');
-const res = require('express/lib/response');
+const express = require("express");
+const connectDB = require("./config/db");
+
 const app = express();
-app.get('/', (req, res) => res.send('API RUNNING')) 
+
+connectDB();
+
+app.get('/', (req, res) => res.send("API running"));
+
 const PORT = process.env.port || 3000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
